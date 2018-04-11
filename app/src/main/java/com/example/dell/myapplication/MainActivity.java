e13b907bc73abf9be359dc2a98551583a2378183
Your Name <you@example.com>
2018-04-11 15:52:08 +0800

    xiong5

--- app/src/main/java/com/example/dell/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dell/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.e("韩高雄","韩高雄");
 
 
     }
